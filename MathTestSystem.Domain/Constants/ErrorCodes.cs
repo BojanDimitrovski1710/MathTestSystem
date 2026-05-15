2113ca8059318d1a7514411c0dbc09cea493aad5
Bojan Dimitrovski <bojan@dimitrovski.net>
2026-05-16 00:35:16 +0200

    feat: added error codes class

---
+++ MathTestSystem.Domain/Constants/ErrorCodes.cs
@@ -0,0 +1,17 @@
+namespace MathTestSystem.Domain.Constants;
+
+public static class ErrorCodes
+{
+    #region Expression
+
+    public const string ExpressionEmpty = "EXPRESSION_EMPTY";
+    public const string ExpressionDivisionByZero = "EXPRESSION_DIVISION_BY_ZERO";
+    public const string ExpressionInvalidCharacter = "EXPRESSION_INVALID_CHARACTER";
+    public const string ExpressionMismatchedParentheses = "EXPRESSION_MISMATCHED_PARENTHESES";
+    public const string ExpressionUnknownToken = "EXPRESSION_UNKNOWN_TOKEN";
+    public const string ExpressionInsufficientOperands = "EXPRESSION_INSUFFICIENT_OPERANDS";
+    public const string ExpressionInvalidStructure = "EXPRESSION_INVALID_STRUCTURE";
+    public const string ExpressionEvaluationFailed = "EXPRESSION_EVALUATION_FAILED";
+
+    #endregion
+}
